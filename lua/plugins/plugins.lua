142fb4dc519b5038c4b801d198f89ee3a180ed93
Marco Caamano <marco.caamano@gmail.com>
2024-09-14 10:25:53 -0600

    cleanup and add markdown preview

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -30,13 +30,6 @@ return {
   -- Rose Pine color theme || https://github.com/rose-pine/neovim
   { "rose-pine/neovim", name = "rose-pine" },
 
-  -- LSP Zero support || https://github.com/VonHeikemen/lsp-zero.nvim
-  -- {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
-  -- {'neovim/nvim-lspconfig'},
-  -- {'hrsh7th/cmp-nvim-lsp'},
-  -- {'hrsh7th/nvim-cmp'},
-  --
-
   -- LSP Using update config from https://lsp-zero.netlify.app/v3.x/blog/theprimeagens-config-from-2022.html
   {'neovim/nvim-lspconfig'},
   {'williamboman/mason.nvim'},
@@ -47,10 +40,21 @@ return {
   {'saadparwaiz1/cmp_luasnip'},
   {'L3MON4D3/LuaSnip'},
 
-  -- These are inherited from the base installation of lazy
-  -- Will need to review and see if I want to keep them
-  "folke/neodev.nvim",
+  -- show available key mappings as popup
   "folke/which-key.nvim",
-  { "folke/neoconf.nvim", cmd = "Neoconf" },
+
+
+  -- markdown preview
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+
 }
 
