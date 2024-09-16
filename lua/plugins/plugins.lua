9dd4edb0e92579098aa07240250f44f29be8739b
Marco Caamano <marco.caamano@gmail.com>
2024-09-16 09:58:11 -0600

    add multiline plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -62,6 +62,8 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' }
   },
 
+  -- Multiline || https://github.com/mg979/vim-visual-multi
+  { 'mg979/vim-visual-multi', branch = 'master' },
 
 }
 
